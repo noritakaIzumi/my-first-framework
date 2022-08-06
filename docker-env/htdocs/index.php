fbe77cd266e78bd94d96ecd9da54662ddba29cbe
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-08-06 09:13:02 +0900

    wip: URI の解析まで

--- docker-env/htdocs/index.php
+++ docker-env/htdocs/index.php
@@ -1,2 +1,26 @@
 <?php
-phpinfo();
+
+const ROOT_PATH = __DIR__;
+
+$parsedUrl = (static function () {
+    $basename = basename(__FILE__);
+    $requestUri = preg_replace(
+        "#/$basename#",
+        '',
+        preg_replace(
+            '/(\/+)/',
+            '/',
+            $_SERVER['REQUEST_URI'],
+        ),
+    );
+
+    return parse_url($requestUri);
+})();
+
+$parsedUrl !== false or trigger_error("request uri is invalid: {$_SERVER['REQUEST_URI']}");
+
+$path = $parsedUrl['path'] ?? '';
+$query = $parsedUrl['query'] ?? '';
+$fragment = $parsedUrl['fragment'] ?? '';
+
+exit;
